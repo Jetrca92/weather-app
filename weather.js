fa62f0ca4ba3921543ad38920b5b36fe7c1c5eb5
Jetrca92 <blaz.guzej@gmail.com>
2023-03-19 18:03:49 +0100

    Add createWeatherInfo function

--- weather.js
+++ weather.js
@@ -1,10 +1,7 @@
 function getWeather(city) {
-    fetch('http://api.weatherapi.com/v1/current.json?key=c72c34d241764345bb3103504231903&q=' + city + '&aqi=no', {mode: 'cors'})
+    return fetch('http://api.weatherapi.com/v1/current.json?key=c72c34d241764345bb3103504231903&q=' + city + '&aqi=no', {mode: 'cors'})
     .then(function(response) {
         return response.json();
-    })
-    .then(function(response) {
-        console.log(response);
     })
     .catch(function(err) {
         console.log('Error!');
@@ -13,6 +10,33 @@ function getWeather(city) {
 
 
 function createWeatherInfo() {
-    let response = getWeather('Celje');
-    console.log(response);
+    const weatherInfo = document.querySelector('.weather-info');
+    const weatherDescription = document.createElement('div');
+    const weatherLocation = document.createElement('div');
+    const weatherDate = document.createElement('div');
+    const weatherTemperature = document.createElement('div');
+    const weatherInfoIcon = document.createElement('div');
+
+    weatherDescription.setAttribute('class', 'weather-description');
+    weatherLocation.setAttribute('class', 'weather-location');
+    weatherDate.setAttribute('class', 'weather-date');
+    weatherTemperature.setAttribute('class', 'weather-temperature');
+    weatherInfoIcon.setAttribute('class', 'weather-info-icon');
+
+     
+    getWeather('Celje')
+    .then(function(response) {
+        weatherDescription.innerHTML = response.current.condition.text;
+        weatherLocation.innerHTML = response.location.name;
+        weatherDate.innerHTML = response.location.localtime;
+        weatherTemperature.innerHTML = response.current.temp_c + " °C";
+        weatherInfoIcon.innerHTML = `<img class="icon" src="https:${response.current.condition.icon}">`;
+    });
+
+    weatherInfo.appendChild(weatherDescription);
+    weatherInfo.appendChild(weatherLocation);
+    weatherInfo.appendChild(weatherDate);
+    weatherInfo.appendChild(weatherTemperature);
+    weatherInfo.appendChild(weatherInfoIcon);
 }
+createWeatherInfo();
